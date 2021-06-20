c8ebf088395e69c021410fb5ba7b78acf1077af6
fheyen <frank.heyen@outlook.de>
2021-06-20 21:10:30 +0200

    new version build

--- dist/musicvislib.js
+++ dist/musicvislib.js
@@ -1,11 +1,11 @@
-// musicvis-lib v0.50.4 https://fheyen.github.io/musicvis-lib
+// musicvis-lib v0.50.5 https://fheyen.github.io/musicvis-lib
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.musicvislib = global.musicvislib || {}));
 }(this, (function (exports) { 'use strict';
 
-  var version="0.50.4";
+  var version="0.50.5";
 
   /**
    * Lookup for many MIDI specifications.
