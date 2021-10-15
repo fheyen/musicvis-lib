1f7d3282f496eca3511466dcb6060f7957f4ace3
fheyen <frank.heyen@outlook.de>
2021-10-15 19:32:46 +0200

    + HarmonicaNote

--- src/types/HarmonicaNote.js
+++ src/types/HarmonicaNote.js
@@ -2,14 +2,15 @@ import { getMidiNoteByLabel } from '../fileFormats/Midi.js';
 import Note from './Note.js';
 
 
+
 /**
- * Guitar note class that reflects MIDI properties but has
+ * Harmonica note class that reflects MIDI properties but has
  * absolute start and end times in seconds and
  * information on how to play it.
  *
  * @augments Note
  */
-class GuitarNote extends Note {
+class HarmonicaNote extends Note {
     /**
      * Creates a new Note
      *
@@ -18,8 +19,8 @@ class GuitarNote extends Note {
      * @param {number} velocity velocity
      * @param {number} channel MIDI channel
      * @param {number} end end time in seconds
-     * @param {number} string guitar string
-     * @param {number} fret guitar fret
+     * @param {number} hole harmonica hole
+     * @param {'blow'|'draw'|'bend'|'overblow'} instruction instruction, e.g., blow or draw
      */
     constructor(
         pitch = 0,
@@ -28,16 +29,16 @@ class GuitarNote extends Note {
         channel = 0,
         end = null,
         // This is different to the base Note class
-        string = null,
-        fret = null,
+        hole = null,
+        instruction = null,
     ) {
         super(pitch, start, velocity, channel, end);
-        this.string = string;
-        this.fret = fret;
+        this.hole = hole;
+        this.instruction = instruction;
     }
 
     /**
-     * Creates a GuitarNote object from an object via destructuring
+     * Creates a HarmonicaNote object from an object via destructuring
      *
      * @param {object} object object with at least {pitch}
      *  {
@@ -46,10 +47,10 @@ class GuitarNote extends Note {
      *      end: number             end time in seconds
      *      velocity: number        MIDI velocity
      *      channel: number         MIDI channel
-     *      string: number          guitar string
-     *      fret: number            guitar fret
+     *      hole: number            harmonica hole
+     *      instruction: string     instruction, e.g., blow or draw
      *  }
-     * @returns {GuitarNote} new note
+     * @returns {HarmonicaNote} new note
      * @throws {Error} when pitch is invalid
      */
     static from(object) {
@@ -60,49 +61,49 @@ class GuitarNote extends Note {
             channel = 0,
             end = null,
             // This is different to the base Note class
-            string = null,
-            fret = null,
+            hole = null,
+            instruction = null,
         } = object;
         if (typeof pitch === 'string' && Number.isNaN(+pitch)) {
             const note = getMidiNoteByLabel(pitch);
             if (note === null || note === undefined) {
-                throw new Error('Invalid pitch for GuitarNote.from()');
+                throw new Error('Invalid pitch for HarmonicaNote.from()');
             }
             pitch = note.pitch;
         }
-        return new GuitarNote(
+        return new HarmonicaNote(
             pitch,
             start,
             velocity,
             channel,
             end,
-            string,
-            fret,
+            hole,
+            instruction,
         );
     }
 
     /**
-     * Converts a Note to a GuitarNote
+     * Converts a Note to a Harmonica
      *
      * @param {Note} note note
-     * @param {number} string string
-     * @param {number} fret fret
-     * @returns {GuitarNote} guitar note
+     * @param {number} hole harmonica hole
+     * @param {'blow'|'draw'|'bend'|'overblow'} instruction instruction, e.g., blow or draw
+     * @returns {HarmonicaNote} harmonica note
      */
-    static fromNote(note, string, fret) {
-        return new GuitarNote(
+    static fromNote(note, hole, instruction) {
+        return new HarmonicaNote(
             note.pitch,
             note.start,
             note.velocity,
             note.channel,
             note.end,
-            string,
-            fret,
+            hole,
+            instruction,
         );
     }
 
     /**
-     * Simplifies the GuitarNote to a Note
+     * Simplifies the HarmonicaNote to a Note
      *
      * @returns {Note} note
      */
@@ -122,15 +123,15 @@ class GuitarNote extends Note {
      * @returns {GuitarNote} new note
      */
     clone() {
-        return new GuitarNote(
+        return new HarmonicaNote(
             this.pitch,
             this.start,
             this.velocity,
             this.channel,
             this.end,
             // This is different to the base Note class
-            this.string,
-            this.fret,
+            this.hole,
+            this.instruction,
         );
     }
 
@@ -141,7 +142,7 @@ class GuitarNote extends Note {
      * @returns {boolean} true if equal
      */
     equals(otherNote) {
-        if (!(otherNote instanceof GuitarNote)) {
+        if (!(otherNote instanceof HarmonicaNote)) {
             return false;
         }
         return (
@@ -151,23 +152,23 @@ class GuitarNote extends Note {
             this.channel === otherNote.channel &&
             this.end === otherNote.end &&
             // This is different to the base Note class
-            this.string === otherNote.string &&
-            this.fret === otherNote.fret
+            this.hole === otherNote.hole &&
+            this.instruction === otherNote.instruction
         );
     }
 
     /**
-     * Human-readable string representation of this GuitarNote
+     * Human-readable string representation of this HarmonicaNote
      *
      * @param {boolean} short if true, attribute names will be shortened
      * @returns {string} string representation
      */
     toString(short = false) {
         if (short) {
-            return `GuitarNote(n: ${this.name}, p: ${this.pitch}, s: ${this.start}, e: ${this.end}, v: ${this.velocity}, c: ${this.channel}, s: ${this.string}, f: ${this.fret})`;
+            return `HarmonicaNote(n: ${this.name}, p: ${this.pitch}, s: ${this.start}, e: ${this.end}, v: ${this.velocity}, c: ${this.channel}, h: ${this.hole}, i: ${this.instruction})`;
         }
-        return `GuitarNote(name: ${this.name}, pitch: ${this.pitch}, start: ${this.start}, end: ${this.end}, velocity: ${this.velocity}, channel: ${this.channel}, string: ${this.string}, fret: ${this.fret})`;
+        return `HarmonicaNote(name: ${this.name}, pitch: ${this.pitch}, start: ${this.start}, end: ${this.end}, velocity: ${this.velocity}, channel: ${this.channel}, hole: ${this.hole}, instruction: ${this.instruction})`;
     }
 }
 
-export default GuitarNote;
+export default HarmonicaNote;
