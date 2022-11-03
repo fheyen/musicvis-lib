1d50d237191139da7f13d9a23b1d576298702a42
fheyen <frank.heyen@outlook.de>
2022-11-03 15:32:41 +0100

    + ColorUtils.setSaturation

--- src/utils/ColorUtils.js
+++ src/utils/ColorUtils.js
@@ -7,7 +7,7 @@ import * as d3 from 'd3'
  * @param {string} color HTML color specifier
  * @returns {number} lightness in [0, 100]
  */
-export function getColorLightness (color) {
+export function getColorLightness(color) {
   const { r, g, b } = d3.color(color).rgb()
   // eslint-disable-next-line no-bitwise
   const Y = (r + r + r + b + g + g + g + g) >> 3
@@ -20,7 +20,7 @@ export function getColorLightness (color) {
  * @param {string[]} colors HTML color specifiers
  * @returns {string} average as RGB string
  */
-export function averageColor (colors) {
+export function averageColor(colors) {
   let mR = 0
   let mG = 0
   let mB = 0
@@ -44,7 +44,21 @@ export function averageColor (colors) {
  * @param {number} [opacity=1] opacity from 0 to 1
  * @returns {string} color as RGBA string
  */
-export function setOpacity (color, opacity = 1) {
+export function setOpacity(color, opacity = 1) {
   const { r, g, b } = d3.color(color).rgb()
   return `rgba(${r}, ${g}, ${b}, ${opacity})`
+}
+
+/**
+ * Sets a color's saturation.
+ * Does not support colors in rgba format.
+ *
+ * @param {string} color valid HTML color identifier
+ * @param {number} [saturation=1] saturation from 0 to 1
+ * @returns {string} color as RGBA string
+ */
+export function setSaturation(color, saturation = 1) {
+  const c = d3.hsl(color)
+  c.s = saturation
+  return c.toString()
 }
